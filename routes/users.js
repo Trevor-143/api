4d027632b46e92fb0aa158066650c8bd0bd69c54
Arapu Trevor <105975921+Trevor-143@users.noreply.github.com>
2023-04-25 22:06:36 +0300

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -9,13 +9,25 @@ const users = [
         age: 27,
         place: "Mukono",
         height: 5.9
+    },
+    {
+        fName: "Oluka",
+        lName: "Martin",
+        age: 30,
+        place: "Nakawa",
+        height: 6
     }
 ]
-
+ 
 //alllll routes here start with /users
 router.get('/', (req, res) => {
-    console.log(users)
-    res.send('These should be ther users.')
+    console.log(users);
+    // res.send('These should be ther users.');
+    res.send(users)
+})
+
+router.post('/', (req, res) => {
+    
 })
 
 export default router;
