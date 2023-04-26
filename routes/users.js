c93f2b743df31d816844f5c09582ec860dcea077
Arapu Trevor <105975921+Trevor-143@users.noreply.github.com>
2023-04-26 14:57:21 +0300

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,16 @@
 import express from 'express';
+import { getUsers, addUser, updateUser, getUser, deleteUser } from "../controllers/users.js";
 
 const router = express.Router();
 
-const users = [
-    {
-        fName: "Arapu",
-        lName: "Trevor",
-        age: 27,
-        place: "Mukono",
-        height: 5.9
-    },
-    {
-        fName: "Oluka",
-        lName: "Martin",
-        age: 30,
-        place: "Nakawa",
-        height: 6
-    }
-]
- 
-//alllll routes here start with /users
-router.get('/', (req, res) => {
-    console.log(users);
-    // res.send('These should be ther users.');
-    res.send(users)
-})
+router.get('/', getUsers)
 
-router.post('/', (req, res) => {
-    
-})
+router.post('/', addUser)
+
+router.patch('/:id', updateUser)
+
+router.get('/:id', getUser)
+
+router.delete('/:id', deleteUser)
 
 export default router;
